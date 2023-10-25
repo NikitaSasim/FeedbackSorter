f1cb7dc90e7335d83962cb4e4169aa468e580ccb
Mikita Sasim <mikita.sasim@student.ehu.lt>
2023-10-26 00:04:30 +0200

    first func

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+import os
+from dotenv import load_dotenv
 import requests
+
+load_dotenv()
 
 
 getFeedbacksUrl = 'https://feedbacks-api.wildberries.ru/api/v1/feedbacks'
+
+apiKey = os.getenv('WB_API_KEY')
+
+
+def getFeedbacks():
+    headers = {
+        "Authorization": apiKey
+    }
+
+    params = {
+        "isAnswered": "true",
+        "take": 80,
+        "skip": 0,
+        "dateFrom": 1698184800,
+        "dateTo": 1698271200
+    }
+
+    response = requests.get(getFeedbacksUrl, params=params, headers=headers)
+    # return response.json().data.feedbacks
+    return response.json()['data']['feedbacks']
+
+
+for feefback in getFeedbacks():
+    if len(feefback['text']) > 0:
+        print(feefback['text'])
+# print(getFeedbacks())
 
 
 
