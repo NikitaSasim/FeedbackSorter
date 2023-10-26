e0f48f52178fe085961849a06ac909c362270dde
Mikita Sasim <mikita.sasim@student.ehu.lt>
2023-10-26 13:18:20 +0200

    add date getter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+from datetime import datetime, timezone, timedelta
 
 load_dotenv()
 
@@ -9,8 +10,42 @@ getFeedbacksUrl = 'https://feedbacks-api.wildberries.ru/api/v1/feedbacks'
 
 apiKey = os.getenv('WB_API_KEY')
 
+def get_dates():
+    today = datetime.now(timezone.utc)
+    # tz = timezone(offset=timedelta())
+    # today = tz.utcoffset(today)
+    date_from = datetime(
+        year=today.year,
+        month=today.month,
+        day=today.day-1,
+        hour=0,
+        minute=0,
+        second=0,
+        tzinfo=timezone.utc
+    )
+    date_to = datetime(
+        year=today.year,
+        month=today.month,
+        day=today.day-1,
+        hour=23,
+        minute=59,
+        second=59,
+        tzinfo=timezone.utc
+    )
+    date_from = int(date_from.timestamp())
+    date_to = int(date_to.timestamp())
+    dates = {
+        'date_from': date_from,
+        'date_to': date_to
+    }
 
-def getFeedbacks():
+    return dates
+
+
+print(get_dates())
+
+
+def get_feedbacks(date_from, date_to):
     headers = {
         "Authorization": apiKey
     }
@@ -19,18 +54,18 @@ def getFeedbacks():
         "isAnswered": "true",
         "take": 80,
         "skip": 0,
-        "dateFrom": 1698184800,
-        "dateTo": 1698271200
+        "dateFrom": date_from,
+        "dateTo": date_to
     }
 
     response = requests.get(getFeedbacksUrl, params=params, headers=headers)
     # return response.json().data.feedbacks
-    return response.json()['data']['feedbacks']
+    feedbacks = response.json()['data']['feedbacks']
+    return feedbacks
 
 
-for feefback in getFeedbacks():
-    if len(feefback['text']) > 0:
-        print(feefback['text'])
+for feefback in get_feedbacks(get_dates()['date_from'], get_dates()['date_to']):
+    print(f"{feefback['createdDate']}    {feefback['id']}")
 # print(getFeedbacks())
 
 
